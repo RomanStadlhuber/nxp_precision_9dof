3388f8d2d6bb9a8538fe9d2c95da8353038146bf
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2024-10-22 11:32:46 +0200

    update IMU node to increase data rate

--- scripts/imu.py
+++ scripts/imu.py
@@ -10,38 +10,44 @@ import adafruit_fxas21002c
 
 
 def imu_talker():
+    rospy.init_node("adafruit_nxp_9dof_imu", anonymous=True)
     # accelerometer and gyroscope
     pub = rospy.Publisher(
         "imu_raw",
         Imu,
-        queue_size=100,
+        queue_size=0,
     )
-    # magnetometer
-    pub_magn = rospy.Publisher(
-        "imu_magnetometer",
-        MagneticField,
-        queue_size=100,
-    )
-    rospy.init_node("adafruit_nxp_9dof_imu", anonymous=True)
-    imu_rate = rospy.get_param("~rate", 500)
-    rate = rospy.Rate(imu_rate)  # [Hz]
+    # whether to publish magnetometer values (defaults to false to increase inertia data rate)
+    should_publish_magnetometer = rospy.get_param("~publish_magnetometer", False)
+    pub_magn = None
+    if should_publish_magnetometer:
+        # magnetometer
+        pub_magn = rospy.Publisher(
+            "imu_magnetometer",
+            MagneticField,
+            queue_size=0,
+        )
+    else:
+        rospy.logwarn("NXP IMU will run in 6DOF mode!")
     # obtain I2C bus connection
     i2c = board.I2C()
     acc_magn_sensor = adafruit_fxos8700.FXOS8700(i2c)
     # acc_magn_sensor = Adafruit_FXOS8700.FXOS8700()
-    rospy.loginfo("NXP-9DOF: acc magn ready")
+    rospy.logdebug("NXP-9DOF: acc magn ready")
     gyro_sensor = adafruit_fxas21002c.FXAS21002C(i2c)
     # gyro_sensor = Adafruit_FXAS21002C.FXAS21002C()
-    rospy.loginfo("NXP-9DOF: gyro ready")
+    rospy.logdebug("NXP-9DOF: gyro ready")
+    # create IMU message buffer for data loop
+    rosimu = Imu()
+    rosimu.header.frame_id = "nxp_9dof_imu"
+
     while not rospy.is_shutdown():
-        rosimu = Imu()
+
+        rosimu.header.stamp = rospy.Time.now()
 
         gyro_x, gyro_y, gyro_z = gyro_sensor.gyroscope
         accel_x, accel_y, accel_z = acc_magn_sensor.accelerometer
-        mag_x, mag_y, mag_z = acc_magn_sensor.magnetometer
 
-        rosimu.header.stamp = rospy.Time.now()
-        rosimu.header.frame_id = "nxp_9dof_imu"
 
         rosimu.angular_velocity.x = gyro_x
         rosimu.angular_velocity.y = gyro_y
@@ -51,17 +57,20 @@ def imu_talker():
         rosimu.linear_acceleration.y = accel_y
         rosimu.linear_acceleration.z = accel_z
 
-        magn_msg = MagneticField()
-        magn_msg.header.stamp = rospy.Time.now()
-        magn_msg.header.frame_id = "nxp_9dof_imu"
-        magn_msg.magnetic_field.x = mag_x
-        magn_msg.magnetic_field.y = mag_y
-        magn_msg.magnetic_field.z = mag_z
-
         pub.publish(rosimu)
-        pub_magn.publish(magn_msg)
-        rate.sleep()
 
+        if pub_magn is not None:
+            mag_x, mag_y, mag_z = acc_magn_sensor.magnetometer
+
+            magn_msg = MagneticField()
+            magn_msg.header.stamp = rospy.Time.now()
+            magn_msg.header.frame_id = "nxp_9dof_imu"
+            magn_msg.magnetic_field.x = mag_x
+            magn_msg.magnetic_field.y = mag_y
+            magn_msg.magnetic_field.z = mag_z
+            pub_magn.publish(magn_msg)
+
+        
 
 if __name__ == "__main__":
     try:
