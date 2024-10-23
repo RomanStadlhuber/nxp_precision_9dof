ee814b686c7b7abe2a850318626545a4920e7f94
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2024-10-23 17:05:03 +0200

    experiment with feature for stable data rate

--- scripts/imu.py
+++ scripts/imu.py
@@ -40,23 +40,50 @@ def imu_talker():
     # create IMU message buffer for data loop
     rosimu = Imu()
     rosimu.header.frame_id = "nxp_9dof_imu"
+    # NOTE: attempt to stabilize the data rate to ~ 200 Hz
+    # stable rate is the rate that can be held with other processes running
+    # i.e. in empirical experiments, the raw data rate should never go
+    # below the stable rate
+    STABLE_RATE = rospy.get_param("stable_rate", 200)  # [Hz]
+    # will not delay for longer than this,
+    # might delay for shorter periods
+    MAX_WAIT_TIME = 1.0 / STABLE_RATE  # [sec]
+    last_update_time = rospy.Time.now()
+    # presumed readout to ROS-publish time
+    # TODO: when refactoring IMU node to class,
+    # experimental perf timing can be conducted before looping
+    READOUT_TIME = 1.0 / 295  # [sec]
 
+    # data readout & publish loop
     while not rospy.is_shutdown():
 
-        rosimu.header.stamp = rospy.Time.now()
+        curr_update_time = rospy.Time.now()
+        # compute how long to wait to keep the stable rate
+        update_diff = (curr_update_time - last_update_time).to_sec()
+        # if update took longer than stable rate, then update_diff < 0 [sec]
+        # otherwise update_diff > 0 [sec]
+        # NOTE: remove readout-time from delay for better rate convergence
+        stable_rate_delay = max(0, MAX_WAIT_TIME - update_diff - READOUT_TIME)
+        delay_duration = rospy.Duration.from_sec(stable_rate_delay)
+        # now delay to obtain the stable rate
+        rospy.sleep(delay_duration)
+        # apply delay to current time and reset last update time
+        curr_update_time += delay_duration
+        last_update_time = curr_update_time
+        # set message readout time
+        rosimu.header.stamp = curr_update_time
 
+        # read out the data and populate IMU message buffer
         gyro_x, gyro_y, gyro_z = gyro_sensor.gyroscope
         accel_x, accel_y, accel_z = acc_magn_sensor.accelerometer
-
-
         rosimu.angular_velocity.x = gyro_x
         rosimu.angular_velocity.y = gyro_y
         rosimu.angular_velocity.z = gyro_z
-
         rosimu.linear_acceleration.x = accel_x
         rosimu.linear_acceleration.y = accel_y
         rosimu.linear_acceleration.z = accel_z
 
+        # publish the data
         pub.publish(rosimu)
 
         if pub_magn is not None:
@@ -70,7 +97,6 @@ def imu_talker():
             magn_msg.magnetic_field.z = mag_z
             pub_magn.publish(magn_msg)
 
-        
 
 if __name__ == "__main__":
     try:
